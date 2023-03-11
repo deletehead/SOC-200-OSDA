a0a827c80d1c02d628670ae18f196d11f81d7953
deletehead <49874223+deletehead@users.noreply.github.com>
2023-03-11 14:52:12 -0500

    Update Get-Sysmon.psm1
    Updated to get smarter specification of params.

--- code/Get-Sysmon.psm1
+++ code/Get-Sysmon.psm1
@@ -1,3 +1,21 @@
-function Get-SysmonEvent {
-    Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational"
+function Get-SysmonEvent{
+    param (
+        $eventid,
+        $start,
+        $end
+    )
+    $filters = @{LogName = "Microsoft-Windows-Sysmon/Operational"}
+    
+    if ($eventid -ne $null) {
+        $filters.ID = $eventid
+    }
+    
+    if ($start -ne $null) {
+        $filters.StartTime = $start
+    }
+
+    if ($end -ne $null) {
+        $filters.EndTime = $end
+    }
+    Get-WinEvent -FilterHashtable $filters
 }
