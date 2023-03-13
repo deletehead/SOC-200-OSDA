0a7af78b9e0708813e56be45d8f20cc351f67ac4
deletehead <49874223+deletehead@users.noreply.github.com>
2023-03-13 15:28:37 -0400

    Update Get-WinEvents.psm1

--- code/Get-WinEvents.psm1
+++ code/Get-WinEvents.psm1
@@ -1,4 +1,9 @@
-### Get a Sysmon event with a specific ID and time
+###
+# Notes:
+# - REMEMBER: Use a 24-hour clock when specifying times. Gets me every time.
+###
+
+# Get a Sysmon event with a specific ID and time
 function Get-SysmonEvent {
     param (
         $eventid,
@@ -21,7 +26,7 @@ function Get-SysmonEvent {
     Get-WinEvent -FilterHashtable $filters
 }
 
-### Get a Security event with a specific ID and time
+# Get a Security event with a specific ID and time
 function Get-SecurityEvent {
     param (
         $eventid,
@@ -42,3 +47,24 @@ function Get-SecurityEvent {
     }
     Get-WinEvent -FilterHashtable $filters
 }
+
+ # Get-SecurityEvent 4625 "5/6/2021 00:00:00" "5/7/2021 00:00:00"
+function Get-FailedLogonEvent {
+    param (
+        $start,
+        $end
+    )
+    $filters = @{LogName = "Security"}
+    
+    # Setting static 4625 for failed logon event
+    $filters.ID = 4625
+
+    if ($start -ne $null) {
+        $filters.StartTime = $start
+    }
+
+    if ($end -ne $null) {
+        $filters.EndTime = $end
+    }
+    Get-WinEvent -FilterHashtable $filters
+} 
